e2d8d8d2488719220780d587976df0936f557ff2
K_ <119788148+K0210@users.noreply.github.com>
2023-08-09 20:16:15 +0900

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,7 @@
+const str1: string = String.fromCodePoint(12522,12450,20805,27515,12397,65281,32)
+const str2: string = String.fromCodePoint(29190,30330,12375,12429,65281)
+const str: string = str1 + str1 + str2
+alert(str1)
+alert(str1)
+alert(str2)
+alert(str)
